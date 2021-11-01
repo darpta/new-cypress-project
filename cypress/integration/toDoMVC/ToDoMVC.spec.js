eafd467d6183bc667884ba50e953ec83045464f4
Dariusz Ptak <dariusz.ptak@op.pl>
2021-11-01 20:59:50 +0100

    Commit Cypress 02

--- cypress/integration/toDoMVC/ToDoMVC.spec.js
+++ cypress/integration/toDoMVC/ToDoMVC.spec.js
@@ -10,4 +10,29 @@ describe('ToDo App', () => {
       cy.get('input').should('have.attr', 'placeholder', 'What needs to be done?')
     })
   })
+  context("When added new item", () => {
+    beforeEach(() => {
+      cy.visit('');
+    })
+    it('should be added only one item', () => {
+        cy.get('input.new-todo').type('Hello!').type('{enter}')
+
+        cy.get('.todo-list li').should('have.length', 1)
+    })
+    it('should add 3 items with proper order', () => {
+        const firstItem = "first Item";
+        const secondItem = "second Item";
+        const thirdItem = "third Item";
+
+        //addNewItem(firstItem)  - jak bedzie dzialac przekazanie funkcji z pliku utils.js
+        cy.get('input.new-todo').type(firstItem).type('{enter}')
+        cy.get('input.new-todo').type(secondItem).type('{enter}')
+        cy.get('input.new-todo').type(thirdItem).type('{enter}')
+
+        cy.get('.todo-list li').eq(0).should('contain.text', firstItem)
+        cy.get('.todo-list li').eq(1).should('contain.text', secondItem)
+        cy.get('.todo-list li').eq(2).should('contain.text', thirdItem)
+
+    })
+  })
 })
